356f81e63674170d85801daf1ebe26d8446ddadb
steveng4x <44285543+steveng4x@users.noreply.github.com>
2022-03-22 15:39:51 +0800

    Add design for hero_cases functionality
    v.0.1

--- src/pages/hero_cases_functionality/CautionReport.js
+++ src/pages/hero_cases_functionality/CautionReport.js
@@ -1,55 +1,88 @@
-import React from 'react';
+import React from "react";
 
 function CautionReport() {
+    let styles = {
+        body: {
+            display: "flex",
+            height: "100vh",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "10px",
+            background: "darkgrey",
+        },
+    };
+
     return (
-        <form>
-            <div>
-                <label for='customerName'>Customer Name*</label>
-                <div>
-                    <input type='text' id='customerName' name='customerName' placeholder='example: Mr Ahmad/Ms Chiu/Mr Rama' />
+        <body style={styles.body}>
+        <div className="hb-container">
+            <div className="hb-title">Caution Report</div>
+            <form action="#">
+                <div className="hb-input-group">
+                    <label className="hb-detail" for="customerName">Customer Name*</label>
+                    <div className="hb-input-box">
+                        <input
+                            type="text"
+                            id="customerName"
+                            name="customerName"
+                            placeholder="example: Mr Ahmad/Ms Chiu/Mr Rama"
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <label for='customerNumber'>Customer Mobile Number*</label>
-                <div>
-                    <input type='tel' id='customerNumber' name='customerName' min={0} placeholder='example: 0123456789' />
+                <div className="hb-input-group">
+                    <label className="hb-detail" for="customerNumber">Customer Mobile Number*</label>
+                    <div className="hb-input-box">
+                        <input
+                            type="tel"
+                            id="customerNumber"
+                            name="customerName"
+                            min={0}
+                            placeholder="example: 0123456789"
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <label for='description'>Description*</label>
-                <div>
-                    <input type='text' id='description' name='userDescription' placeholder='example: Need Help with abcd@unifi or Sales Lead Package unifi 100mbps' />
+                <div className="hb-input-group">
+                    <label className="hb-detail" for="description">Description*</label>
+                    <div className="hb-input-box">
+                        <input
+                            type="text"
+                            id="description"
+                            name="userDescription"
+                            placeholder="example: Need Help with abcd@unifi or Sales Lead Package unifi 100mbps"
+                        />
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <label for='type'>Type*</label>
-
-                <div id='type' name='caution_report'>
-                    <p value='caution report'>Assurance</p>
+                <div className="hb-input-group">
+                    <label className="hb-detail" for="type">Type*</label>
+                    <div className="hb-input-box" id="type" name="caution_report">
+                        <p value="caution report">Assurance</p>
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <label for='location'>Location*</label>
-
-                <div>
-                    <select id='location' name='location'>
-                        <option value='empty'>Select one</option>
-                    </select>
+                <div className="hb-input-group">
+                    <label className="hb-detail" for="location">Location*</label>
+                    <div className="hb-input-box">
+                        <select id="location" name="location">
+                            <option value="empty">Select one</option>
+                        </select>
+                    </div>
                 </div>
-            </div>
 
-            <div>
-                <h5>Attachment</h5>
-                <input type='file' name='imageAttach' />
-            </div>
-
-            <input type='submit' title='Submit' />
-        </form>
-    )
+                <div className="hb-input-group">
+                    <label className="hb-detail">Attachment</label>
+                    <div className="hb-attachment">
+                        <input type="file" name="imageAttach"/>
+                    </div>
+                </div>
+                <div className="hb-button">
+                    <input className="hb-submit" type="submit" title="Submit"/>
+                </div>
+            </form>
+        </div>
+        </body>
+    );
 }
 
 export default CautionReport;
