33392867b063d253a55c0c37f32d1586073430cc
steveng4x <44285543+steveng4x@users.noreply.github.com>
2022-03-25 14:34:10 +0800

    Quick fix on AllAssignmentTableData
    - fix on filter not working as intended

--- src/pages/all_assignments/AA_Assigned.js
+++ src/pages/all_assignments/AA_Assigned.js
@@ -1,78 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import Layout from '../Layout';
 import AllAssignmentTable from '../../components/assignments/AllAssignmentTableData';
 import AssignmentService from '../../web_service/assignment_service/MyAssignmentService';
 
 
 function AA_Assigned() {
-  const [lovData, setLovData] = useState(JSON.parse(sessionStorage.getItem('LovData')));
-  const [token, setToken] = useState(JSON.parse(sessionStorage.getItem('userToken')));
-  const [userData, setUserData] = useState(JSON.parse(sessionStorage.getItem('UserData')));
-  const [assignedData, setAssignedCase] = useState([]);
-  const [caseType, setCaseType] = useState('0');
-  const [groupType, setGroupType] = useState('0');
+    const [lovData, setLovData] = useState(JSON.parse(sessionStorage.getItem('LovData')));
+    const [token, setToken] = useState(JSON.parse(sessionStorage.getItem('userToken')));
+    const [userData, setUserData] = useState(JSON.parse(sessionStorage.getItem('UserData')));
+    const [assignedData, setAssignedCase] = useState([]);
+    const [caseType, setCaseType] = useState('0');
+    const [groupType, setGroupType] = useState('0');
 
-  useEffect(() => {
-    const allAssignmentData = () => {
-      AssignmentService.viewCaseByGroup(token, userData.shID, 64).then(res => {
-        // console.log(res);
-        setAssignedCase(res)
-      })
-    }
+    useEffect(() => {
+        const allAssignmentData = () => {
+            AssignmentService.viewCaseByGroup(token, userData.shID, 64).then(res => {
+                // console.log(res);
+                setAssignedCase(res)
+            })
+        }
+        allAssignmentData();
+    }, [])
 
-    allAssignmentData();
-  }, [])
 
+    return (
+        <Layout
+            pageTitle={
+                <span>
+                    Nationwide Assignments : <span style={{color: 'green'}}>ASSIGNED</span>
+                </span>
+            }
+            pageContent={
+                <div className="row">
+                    <form name="form">
+                        <div className="col-sm-3">
+                            <select className="chosen-select form-control" name="shID" value={groupType}
+                                    onChange={(e) => setGroupType(e.target.value)}>
+                                <option value='0'>All Group/Stakeholder ...</option>
+                                {
+                                    lovData
+                                        .filter(filter => filter.lovGroup === 'STAKEHOLDER' && filter.lovName !== 'ADMIN')
+                                        .map((data, key) => <option key={key} value={data.lovName}>{data.lovName} </option>)
+                                }
+                            </select>
+                        </div>
 
-  return (
-    <Layout
-      pageTitle={
-        <span>
-          Nationwide Assignments : <span style={{color: 'green'}}>ASSIGNED</span>
-        </span>
-      }
-      pageContent={
-          <div className="row">
-            <form name="form">
-              <div className="col-sm-3">
-                <select className="chosen-select form-control" name="shID" value={groupType} onChange={(e) => setGroupType(e.target.value)}>
-                  <option value='0'>All Group/Stakeholder ...</option>
-                  {lovData.filter(filter => filter.lovGroup === 'STAKEHOLDER' && filter.lovName !== 'ADMIN').map((data, key) =>
-                    <option key={key} value={data.lovName}> {data.lovName} </option>
-                  )
-                  }
-                </select>
-              </div>
+                        <div className="col-sm-3">
+                            <select className="chosen-select form-control" name="caseTypeID" value={caseType}
+                                    onChange={(e) => setCaseType(e.target.value)}>
+                                <option value='0'>All Case Type ...</option>
+                                {
+                                    lovData
+                                        .filter(filter => filter.lovGroup === 'CASE-TYPE')
+                                        .map((data, key) => <option key={key} value={data.lovName}>{data.lovName}</option>)
+                                }
+                            </select>
+                        </div>
+                    </form>
 
-              <div className="col-sm-3">
-                <select className="chosen-select form-control" name="caseTypeID" value={caseType} onChange={(e) => setCaseType(e.target.value)}>
-                  <option value='0'>All Case Type ...</option>
-                  {
-                    lovData.filter(filter => filter.lovGroup === 'CASE-TYPE').map((data, key) =>
-                      <option key={key} value={data.lovName}>{data.lovName}</option>
-                    )
-                  }
-                </select>
-              </div>
-
-            </form>
-
-            <div className="col-xs-12">
-              <div className="clearfix">
-                <div className="pull-right tableTools-container" />
-              </div>
-              <div>
-                <AllAssignmentTable
-                  tableData={assignedData}
-                  caseType={caseType}
-                  groupType={groupType}
-                />
-              </div>
-            </div>  {/* //<!-- /.span --> */}
-          </div>
-      }
-    />
-  );
+                    <div className="col-xs-12">
+                        <div className="clearfix">
+                            <div className="pull-right tableTools-container"/>
+                        </div>
+                        <div>
+                            <AllAssignmentTable
+                                tableData={assignedData}
+                                caseType={caseType}
+                                groupType={groupType}
+                            />
+                        </div>
+                    </div>
+                    {/* //<!-- /.span --> */}
+                </div>
+            }
+        />
+    );
 }
 
 export default AA_Assigned;
