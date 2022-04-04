f691aea1189a3adf38e7ccb4eb1c4f1788fe23e0
steveng4x <44285543+steveng4x@users.noreply.github.com>
2022-04-04 12:20:09 +0800

    Changes to LoginPage
    - css change for responsive behaviours

--- src/components/assignments/AllAssignmentTableData.js
+++ src/components/assignments/AllAssignmentTableData.js
@@ -1,9 +1,10 @@
-import React, {useMemo, useState} from "react";
-import {Link} from 'react-router-dom';
+import React, { useMemo, Suspense, Spinner } from "react";
+import { Link } from 'react-router-dom';
+
 
 function AllAssignmentTable(props) {
-    // let prepData = useMemo(() => props, []);
-    let filteredData = props.tableData
+    let prepData = useMemo(() => props, [props]);
+    let filteredData = prepData.tableData
         .filter((item) =>
             (item.caseType === props.caseType && item.stakeholderName === props.groupType)
             || (props.caseType === item.caseType && props.groupType === "0")
@@ -55,64 +56,65 @@ function AllAssignmentTable(props) {
     let tBody;
     if (filteredData.length !== 0) {
         tBody =
-            <tbody>
-            {
-                filteredData.map((data, index) => {
-                    const date = new Date(data.createdDate)
-                    const formattedDate = date.toLocaleDateString("en-GB", {
-                        day: "numeric",
-                        month: "2-digit",
-                        year: "numeric",
-                        hour: 'numeric',
-                        minute: 'numeric',
-                        hourCycle: 'h12'
-                    })
-                    const agingDay = (data.caseStatus === 'CLOSED') ? data.closedAging : data.unclosedAging;
-                    const agingKey = (data.caseStatus === 'CLOSED') ? data.closedAgingDH : data.unclosedAgingDH;
-                    return (
-                        <tr key={index}>
-                            <td>
-                                <Link to={`/case-detail/${data.cToken}`}>
-                                    {data.caseNum}
-                                </Link>
-                                <br/>
-                                <small title="Created Date">{formattedDate}</small>
-                            </td>
-                            <td>
-                                <div align="center">
-                                    <span className='badge badge-info'>{data.caseStatus ? 'A' : '-'}</span>
-                                </div>
-                            </td>
-                            <td>
-                                <div align="center" title='Day:Hour'>
-                                    {agingDay < 16 ? agingKey : <span style={{fontSize: "10px"}}
-                                                                      className={`badge badge-sm badge-${data.unclosedAging > 30 ? 'danger' : 'warning'}`}>{agingKey}</span>}
-                                </div>
-                            </td>
-                            <td>{data.caseType}</td>
-                            <td>
-                                <div align="center">
-                                    {data.vip ? <i className="menu-icon glyphicon glyphicon-ok"></i> : '-'}
-                                </div>
-                            </td>
-                            <td>
-                                <div align="center">{data.eligibility}</div>
-                            </td>
-                            <td>{data.productName}</td>
-                            <td>{data.customerName}</td>
-                            <td>{data.vip ? <span
-                                className="label label-success arrowed-right">{data.fullname}</span> : data.fullname}</td>
-                            <td>{data.ownerName === null ? 'Un - Assigned - ' + data.stakeholderName : data.ownerName + ' - ' + data.stakeholderName}</td>
-                            <td>
-                                <div align="center">{data.areaLocation}</div>
-                            </td>
-                            <td>
-                                <div align="center" style={{fontSize: 10}}>
-                                    {data.totalNewAlert > 0 ? <span style={{fontSize: 10}}
-                                                                    className="badge badge-warning">{data.totalNewAlert}</span> : '0'}
-                                </div>
-                            </td>
-                            {/* <td>
+            <Suspense fallback={<Spinner />}>
+                <tbody>
+                {
+                    filteredData.map((data, index) => {
+                        const date = new Date(data.createdDate)
+                        const formattedDate = date.toLocaleDateString("en-GB", {
+                            day: "numeric",
+                            month: "2-digit",
+                            year: "numeric",
+                            hour: 'numeric',
+                            minute: 'numeric',
+                            hourCycle: 'h12'
+                        })
+                        const agingDay = (data.caseStatus === 'CLOSED') ? data.closedAging : data.unclosedAging;
+                        const agingKey = (data.caseStatus === 'CLOSED') ? data.closedAgingDH : data.unclosedAgingDH;
+                        return (
+                            <tr key={index}>
+                                <td>
+                                    <Link to={`/case-detail/${data.cToken}`}>
+                                        {data.caseNum}
+                                    </Link>
+                                    <br/>
+                                    <small title="Created Date">{formattedDate}</small>
+                                </td>
+                                <td>
+                                    <div align="center">
+                                        <span className='badge badge-info'>{data.caseStatus ? 'A' : '-'}</span>
+                                    </div>
+                                </td>
+                                <td>
+                                    <div align="center" title='Day:Hour'>
+                                        {agingDay < 16 ? agingKey : <span style={{fontSize: "10px"}}
+                                                                          className={`badge badge-sm badge-${data.unclosedAging > 30 ? 'danger' : 'warning'}`}>{agingKey}</span>}
+                                    </div>
+                                </td>
+                                <td>{data.caseType}</td>
+                                <td>
+                                    <div align="center">
+                                        {data.vip ? <i className="menu-icon glyphicon glyphicon-ok"></i> : '-'}
+                                    </div>
+                                </td>
+                                <td>
+                                    <div align="center">{data.eligibility}</div>
+                                </td>
+                                <td>{data.productName}</td>
+                                <td>{data.customerName}</td>
+                                <td>{data.vip ? <span
+                                    className="label label-success arrowed-right">{data.fullname}</span> : data.fullname}</td>
+                                <td>{data.ownerName === null ? 'Un - Assigned - ' + data.stakeholderName : data.ownerName + ' - ' + data.stakeholderName}</td>
+                                <td>
+                                    <div align="center">{data.areaLocation}</div>
+                                </td>
+                                <td>
+                                    <div align="center" style={{fontSize: 10}}>
+                                        {data.totalNewAlert > 0 ? <span style={{fontSize: 10}}
+                                                                        className="badge badge-warning">{data.totalNewAlert}</span> : '0'}
+                                    </div>
+                                </td>
+                                {/* <td>
                             <div align="center">
                               <Link className="btn btn-minier btn-yellow" to={`/hero-chat/${data.cToken}`}>
                                 Open
@@ -120,11 +122,12 @@ function AllAssignmentTable(props) {
                               </Link>
                             </div>
                           </td> */}
-                        </tr>
-                    )
-                })
-            }
-            </tbody>
+                            </tr>
+                        )
+                    })
+                }
+                </tbody>
+            </Suspense>
     } else {
         tBody =
             <tbody>
