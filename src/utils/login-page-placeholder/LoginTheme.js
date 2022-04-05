2dceb17236071a004c594fc2742ffa737c90e16f
steveng4x <44285543+steveng4x@users.noreply.github.com>
2022-04-05 10:04:33 +0800

    Update on LoginTheme.js
    - revert back main-container, main-content, row

--- src/utils/login-page-placeholder/LoginTheme.js
+++ src/utils/login-page-placeholder/LoginTheme.js
@@ -17,22 +17,28 @@ class LoginTheme extends React.Component {
     render() {
         return (
             <div class="login-layout">
-                <div className="login-container">
-                    <div className="login-header" style={{paddingTop: 20}}>
-                        <img className="login-logo" src={logo}/>
-                        <h5 className="red" id="id-company-text">
-                            HERO PORTAL For Case Resolution by Internal Team
-                        </h5>
-                    </div>
-                    <div class="login-box">
-                        {this.props.children}
-                    </div>
-                    <div className="footer">
-                        <div className="copyright">
-                            Copyright © 2018 Telekom Malaysia Berhad (128740-P) ALL RIGHTS RESERVED
-                            <br/>
-                            For the best viewing experience, please use either Mozilla Firefox or IE browser
-                            with resolution at 1280 x 800 pixels and above
+                <div className="main-container">
+                    <div className="main-content">
+                        <div className="row">
+                            <div className="login-container">
+                                <div className="login-header" style={{paddingTop: 20}}>
+                                    <img className="login-logo" src={logo}/>
+                                    <h5 className="red" id="id-company-text">
+                                        HERO PORTAL For Case Resolution by Internal Team
+                                    </h5>
+                                </div>
+                                <div class="login-box">
+                                    {this.props.children}
+                                </div>
+                                <div className="footer">
+                                    <div className="copyright">
+                                        Copyright © 2018 Telekom Malaysia Berhad (128740-P) ALL RIGHTS RESERVED
+                                        <br/>
+                                        For the best viewing experience, please use either Mozilla Firefox or IE browser
+                                        with resolution at 1280 x 800 pixels and above
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
