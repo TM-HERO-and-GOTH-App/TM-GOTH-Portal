01d7d801e4315d6f3a93dd159e5c2c9b6e083161
steveng4x <44285543+steveng4x@users.noreply.github.com>
2022-04-08 12:19:42 +0800

    Add Export Feature
    -Add an extra feature to AllAsignmentTable

--- src/components/assignments/AllAssignmentTableData.js
+++ src/components/assignments/AllAssignmentTableData.js
@@ -1,6 +1,6 @@
 import React, { useMemo, Suspense, Spinner } from "react";
 import { Link } from 'react-router-dom';
-
+import { CSVLink } from 'react-csv';
 
 function AllAssignmentTable(props) {
     let prepData = useMemo(() => props, [props]);
@@ -140,6 +140,15 @@ function AllAssignmentTable(props) {
     }
 
     return (
+        <div className="table-container">
+            <div className="pull-right tableTools-container dt-buttons btn-overlap btn-group">
+            <CSVLink
+                className="buttons-csv buttons-html5 btn btn-white btn-primary btn-bold pull-right"
+                data={filteredData} filename={"assignment-table.csv"}
+            >
+                <i className="fa fa-database bigger-110 orange"/> Export to CSV
+            </CSVLink>
+            </div>
         <table id="dynamic-table"
                className="table table-striped table-bordered table-hover">
             {/* <!-- id="simple-table" className="table table-bordered table-hover" --> */}
@@ -172,6 +181,7 @@ function AllAssignmentTable(props) {
             </thead>
             {tBody}
         </table>
+        </div>
     )
 }
 
