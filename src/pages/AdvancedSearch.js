16f78b960fcc9560b1a53b3b2fee64b934fe30ea
Aiman1017 <aimanirfan1017@gmail.com>
2022-04-18 16:53:45 +0800

    Add Custom Calendar UI

--- src/pages/AdvancedSearch.js
+++ src/pages/AdvancedSearch.js
@@ -20,13 +20,270 @@ function AdvancedSearch(props) {
   const [customerNameInput, setCustomerNameInput] = useState('');
   const [srNumberInput, setSRNumberInput] = useState('');
   const [ttNumberInput, setTTNumberInput] = useState('');
+  const [calendarDays, setCalendarDays] = useState('');
+  const [calendarMonth, setCalendarMonth] = useState('');
   const [searchResult, setSearchResult] = useState([]);
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [nextMonth, setNextMonth] = useState(false);
+  const [previousMonth, setPreviousMonth] = useState(false);
+
+  const Calendar = () => {
+    return (
+      <div className={`datepicker datepicker-dropdown dropdown-menu datepicker-orient-left datepicker-orient-bottom ${showCalendar ? "datepicker-dropdown:after datepicker-orient-left:after datepicker-orient-bottom:after" : "datepicker-orient-bottom:before datepicker-orient-left:before datepicker-dropdown:before"}`} 
+      style={{ marginTop: "50px", marginLeft: "11.5px", display: "block" }}><div className="datepicker-days" style={{ display: "block" }}>
+        <table className=" table-condensed">
+          <thead>
+            <tr>
+              <th colSpan="7" className="datepicker-title" style={{ display: "none" }}>
+              </th>
+            </tr>
+            <tr>
+              <th className="prev" style={{ visibility: "visible" }}>«</th>
+              <th colSpan="5" className="datepicker-switch">April 2022</th>
+              <th className="next" style={{ visibility: "visible" }} onClick={() => alert('forward button pressed')}>»</th>
+            </tr>
+            <tr>
+              <th className="dow">Su</th>
+              <th className="dow">Mo</th>
+              <th className="dow">Tu</th>
+              <th className="dow">We</th>
+              <th className="dow">Th</th>
+              <th className="dow">Fr</th>
+              <th className="dow">Sa</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="old day">27</td>
+              <td className="old day">28</td>
+              <td className="old day">29</td>
+              <td className="old day">30</td>
+              <td className="old day">31</td>
+              <td className="day">1</td>
+              <td className="day">2</td>
+            </tr>
+            <tr>
+              <td className="day">3</td>
+              <td className="day">4</td>
+              <td className="day">5</td>
+              <td className="day">6</td>
+              <td className="day">7</td>
+              <td className="day">8</td>
+              <td className="day">9</td>
+            </tr>
+            <tr>
+              <td className="day">10</td>
+              <td className="day">11</td>
+              <td className="day">12</td>
+              <td className="day">13</td>
+              <td className="day">14</td>
+              <td className="day">15</td>
+              <td className="today day">16</td>
+            </tr>
+            <tr>
+              <td className="day">17</td>
+              <td className="day">18</td>
+              <td className="day">19</td>
+              <td className="day">20</td>
+              <td className="day">21</td>
+              <td className="day">22</td>
+              <td className="day">23</td>
+            </tr>
+            <tr>
+              <td className="day">24</td>
+              <td className="day">25</td>
+              <td className="day">26</td>
+              <td className="day">27</td>
+              <td className="day">28</td>
+              <td className="day">29</td>
+              <td className="day">30</td>
+            </tr>
+            <tr>
+              <td className="new day">1</td>
+              <td className="new day">2</td>
+              <td className="new day">3</td>
+              <td className="new day">4</td>
+              <td className="new day">5</td>
+              <td className="new day">6</td>
+              <td className="new day">7</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan="7" className="today" style={{ display: "none" }}>Today</th>
+            </tr>
+            <tr>
+              <th colSpan="7" className="clear" style={{ display: "none" }}>Clear</th>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+        <div className="datepicker-months" style={{ display: "none" }}>
+          <table className="table-condensed">
+            <thead>
+              <tr>
+                <th colSpan="7" className="datepicker-title" style={{ display: "none" }}></th>
+              </tr>
+              <tr>
+                <th className="prev" style={{ visibility: "visible" }}>«</th>
+                <th colSpan="5" className="datepicker-switch">2022</th>
+                <th className="next" style={{ visibility: "visible" }}>»</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan="7">
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Jan</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Feb</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Mar</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Apr</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>May</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Jun</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Jul</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Aug</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Sep</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Oct</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Nov</span>
+                  <span className={nextMonth || previousMonth ? "month focused" : "month"}>Dec</span>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th colSpan="7" className="today" style={{ display: "none" }}>Today</th>
+              </tr>
+              <tr>
+                <th colSpan="7" className="clear" style={{ display: "none" }}>Clear</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div className="datepicker-years" style={{ display: "none" }}><table className="table-condensed">
+          <thead>
+            <tr>
+              <th colSpan="7" className="datepicker-title" style={{ display: "none" }}></th>
+            </tr>
+            <tr>
+              <th className="prev" style={{ visibility: "visible" }}>«</th>
+              <th colSpan="5" className="datepicker-switch">2020-2029</th>
+              <th className="next" style={{ visibility: "visible" }}>»</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td colSpan="7"><span className="year old">2019</span><span className="year">2020</span>
+                <span className="year">2021</span>
+                <span className="year focused">2022</span>
+                <span className="year">2023</span>
+                <span className="year">2024</span>
+                <span className="year">2025</span>
+                <span className="year">2026</span>
+                <span className="year">2027</span>
+                <span className="year">2028</span>
+                <span className="year">2029</span>
+                <span className="year new">2030</span>
+              </td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <th colSpan="7" className="today" style={{ display: "none" }}>Today</th>
+            </tr>
+            <tr>
+              <th colSpan="7" className="clear" style={{ display: "none" }}>Clear</th>
+            </tr>
+          </tfoot>
+        </table>
+        </div>
+        <div className="datepicker-decades" style={{ display: "none" }}>
+          <table className="table-condensed">
+            <thead>
+              <tr>
+                <th colSpan="7" className="datepicker-title" style={{ display: "none" }}></th>
+              </tr>
+              <tr>
+                <th className="prev" style={{ visibility: "visible" }}>«</th>
+                <th colSpan="5" className="datepicker-switch">2000-2090</th>
+                <th className="next" style={{ visibility: "visible" }}>»</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan="7">
+                  <span className="decade old">1990</span>
+                  <span className="decade">2000</span>
+                  <span className="decade">2010</span>
+                  <span className="decade">2020</span>
+                  <span className="decade">2030</span>
+                  <span className="decade">2040</span>
+                  <span className="decade">2050</span>
+                  <span className="decade">2060</span>
+                  <span className="decade">2070</span>
+                  <span className="decade">2080</span>
+                  <span className="decade">2090</span>
+                  <span className="decade new">2100</span>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th colSpan="7" className="today" style={{ display: "none" }}>Today</th>
+              </tr>
+              <tr>
+                <th colSpan="7" className="clear" style={{ display: "none" }}>Clear</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+        <div className="datepicker-centuries" style={{ display: "none" }}>
+          <table className="table-condensed">
+            <thead>
+              <tr>
+                <th colSpan="7" className="datepicker-title" style={{ display: "none" }}></th>
+              </tr>
+              <tr>
+                <th className="prev" style={{ visibility: "visible" }}>«</th>
+                <th colSpan="5" className="datepicker-switch">2000-2900</th>
+                <th className="next" style={{ visibility: "visible" }}>»</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td colSpan="7">
+                  <span className="century old">1900</span>
+                  <span className="century">2000</span>
+                  <span className="century">2100</span>
+                  <span className="century">2200</span>
+                  <span className="century">2300</span>
+                  <span className="century">2400</span>
+                  <span className="century">2500</span>
+                  <span className="century">2600</span>
+                  <span className="century">2700</span>
+                  <span className="century">2800</span>
+                  <span className="century">2900</span>
+                  <span className="century new">3000</span>
+                </td>
+              </tr>
+            </tbody>
+            <tfoot>
+              <tr>
+                <th colSpan="7" className="today" style={{ display: "none" }}>Today</th>
+              </tr>
+              <tr>
+                <th colSpan="7" className="clear" style={{ display: "none" }}>Clear</th>
+              </tr>
+            </tfoot>
+          </table>
+        </div>
+      </div>
+    )
+  }
 
   // TODO: fix advance search caseType and HERO Group API
   const advancedSearch = (e) => {
     e.preventDefault();
     AdvancedSearchService.advancedSearch(token, emailInput, fullNameInput, nricInput, srNumberInput,
-      ttNumberInput, caseNumberInput, vipNameInput, customerNameInput, caseType, startDateInput, endDateInput,  heroGroupType)
+      ttNumberInput, caseNumberInput, vipNameInput, customerNameInput, caseType, startDateInput, endDateInput, heroGroupType)
       .then(res => {
         console.log(res);
         setSearchResult(res)
@@ -56,16 +313,16 @@ function AdvancedSearch(props) {
           <div className="col-sm-12">Please enter your keywords...</div>
           <div className="space-2" />
 
-          <div class="row">
+          <div className="row">
             <form name="form" onSubmit={advancedSearch} onReset={reset}>
               <div className="col-sm-2">
                 <div className="input-group">
-                  <input className="form-control date-picker" id="id-date-picker-1" name="keyStartDate" type='date' style={{ width: 180 }} data-date-format="yyyy-mm-dd" value={startDateInput} onChange={(e) => setStartDateInput(e.currentTarget.val)} placeholder="Date Created (Start)" />
-                  <span className="input-group-addon">
-                    <i className="fa fa-calendar bigger-110" onClick={() => {
-                      const startDate = document.getElementById('id-date-picker-1');
-                      startDate.focus()
-                    }} />
+                  <input className="form-control date-picker" id="id-date-picker-1" name="keyStartDate" type='text' style={{ width: 180 }} data-date-format="yyyy-mm-dd" value={startDateInput} onChange={(e) => setStartDateInput(e.currentTarget.valueAsDate)} placeholder="Date Created (Start)" />
+                  <span className="input-group-addon" onClick={() =>
+                    setShowCalendar(!showCalendar)
+                    // alert('Calendar Icon being clicked')
+                  }>
+                    <i className="fa fa-calendar bigger-110" />
                   </span>
                 </div>
               </div>
@@ -74,7 +331,7 @@ function AdvancedSearch(props) {
                 <div className="input-group">
                   <input className="form-control date-picker" id="id-date-picker-1" name="keyEndDate" type="date" style={{ width: "180px" }} data-date-format="yyyy-mm-dd" value={endDateInput} onChange={(e) => setEndDateInput(e.currentTarget.value)} placeholder="Date Created (End)" />
                   <span className="input-group-addon">
-                    <i className="fa fa-calendar bigger-110"></i>
+                    <i className="fa fa-calendar bigger-110" />
                   </span>
                 </div>
               </div>
@@ -99,8 +356,8 @@ function AdvancedSearch(props) {
                   <option value="40">UniFi Mobile</option>
                   <option value="521">Unifi Public Wifi</option>
                 </select>
-                
-                  {/* <div className="chosen-drop">
+
+                {/* <div className="chosen-drop">
                     <div className="chosen-search">
                       <input type="text" autocomplete="off" />
                     </div>
@@ -111,7 +368,7 @@ function AdvancedSearch(props) {
               </div>
 
               <div className="col-sm-2 chosen-container chosen-container-single" style={{ paddingLeft: 10 }}>
-                <select className="chosen-select form-control" name="keyHeroGroup" value={heroGroupType} onChange={(e) => setHeroGroupType(e.currentTarget.value)} data-placeholder="HERO Group..."  style={{ width: "220px" }}>
+                <select className="chosen-select form-control" name="keyHeroGroup" value={heroGroupType} onChange={(e) => setHeroGroupType(e.currentTarget.value)} data-placeholder="HERO Group..." style={{ width: "220px" }}>
                   <option value="0">HERO Group...</option>
                   <option value="WKTM">WKTM</option>
                   <option value="TMCC">TMCC</option>
@@ -201,7 +458,7 @@ function AdvancedSearch(props) {
               </thead>
               <tbody>
                 {
-                  (searchResult.length === 0 || searchResult === undefined)?
+                  (searchResult.length === 0 || searchResult.length === undefined) ?
                     <tr><td colSpan={14}><span style={{ color: 'red' }}>Search result is empty</span></td></tr>
                     :
                     searchResult.map((data, index) => {
@@ -261,7 +518,7 @@ function AdvancedSearch(props) {
                           <td>{data.productName}</td>
                           <td>{data.customerName}</td>
                           <td>{data.vip ? <span className="label label-success arrowed-right">{data.fullname}</span> : data.fullname}</td>
-                          <td>{data.ownerName} <span class="label label-lg label-info arrowed-right">
+                          <td>{data.ownerName} <span className="label label-lg label-info arrowed-right">
                             <small>
                               {data.stakeholderName}
                             </small>
@@ -288,6 +545,8 @@ function AdvancedSearch(props) {
             </table>
           </div>{/* /.span */}
           {/* /.row */}
+          <div id="limiterBox" className="limiterBox" style={{ position: "absolute", display: "none" }}/>
+          {showCalendar && <Calendar />}
         </>
       }
     />
