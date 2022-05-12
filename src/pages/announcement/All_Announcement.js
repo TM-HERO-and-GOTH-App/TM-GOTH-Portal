c297621b234ee4f3fe529acf1875f0860852f4f9
Aiman1017 <aimanirfan1017@gmail.com>
2022-05-12 15:20:03 +0800

    Fix Announcement route, Change Announcement Card layout, change Announcement Form folder and add back button and Add Announcement tab in Layout file, add Fiirebase file in gitIgnore and fix JS script tag in index.html

--- src/pages/announcement/All_Announcement.js
+++ src/pages/announcement/All_Announcement.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import AnnouncementCard from "../../components/announcement/Announcement_Card";
 import Layout from "../Layout";
 
 export default function All_Announcement() {
+    const [moreText, setMoreText] = useState(false);
     return (
         <Layout
             pageTitle="All Announcements"
             pageContent={
                 <div style={{ margin: "10px 0 0 100px"}}>
-                    <AnnouncementCard/>
+                    <AnnouncementCard showMoreButton={() => setMoreText(!moreText)} showMore={moreText}/>
                 </div>
             }
         />
