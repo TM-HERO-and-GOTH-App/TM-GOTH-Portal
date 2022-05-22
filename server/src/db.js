1227a433258a91f0362974dae71129d5a99b6eb1
steveng4x <44285543+steveng4x@users.noreply.github.com>
2022-05-22 15:29:28 +0800

    update on chat api

--- server/src/db.js
+++ server/src/db.js
@@ -1,11 +1,20 @@
-const mariadb = require('mariadb');
+const mysql = require('mysql');
 
 // Expose the Pool object within this module
-module.exports = Object.freeze({
-    pool: mariadb.createPool({
+const db_config = {
+    localPool: mysql.createPool({
         host: 'localhost',
         port: '3306',
         user: 'root',
         password: '1234',
+        database: 'emdev'
+    }),
+    pool: mysql.createPool({
+        host: "172.20.197.194",
+        port: 3306,
+        user: "hero_app",
+        password: "Pswd2022",
+        database: "HERO2.0_DEV"
     })
-});
+}
+module.exports = db_config
--- server/src/test/connect.js
+++ server/src/test/connect.js
@@ -1,25 +1,28 @@
 const mariadb = require('mariadb');
+const mysql = require('mysql');
+const db = require('../db');
 
 async function asyncFunction() {
     let conn;
-    try {
-        // Create a new connection
-        conn = await mariadb.createConnection({
-            host: 'localhost',
-            port: '3306',
-            user: 'root',
-            password: '1234',
-        });
-
-        // Print connection thread
-        console.log(`Connected! (id=${conn.threadId})`);
-    } catch (err) {
-        // Print error
-        console.log(err);
-    } finally {
-        // Close connection
-        if (conn) await conn.close();
-    }
+    conn = await mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: '1234',
+        database: 'emdev'
+    });
+    await conn.connect(function(err) {
+        if (err) {
+            console.error(`error connecting: ${err.stack}`)
+        }
+        console.log(`connected to id ('${conn.threadId}')`)
+    })
+    await conn.end();
+    // await conn.query('select * from tbl_chat',
+    //     function (err, res) {
+    //         if (err) throw err;
+    //         // connected
+    //         console.log(res);
+    //     })
 }
-
 asyncFunction();
