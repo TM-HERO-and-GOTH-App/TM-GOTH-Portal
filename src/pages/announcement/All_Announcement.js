64dfa998b75560b7bac4817e38c3f573608dae27
Aiman1017 <aimanirfan1017@gmail.com>
2022-06-14 15:26:35 +0800

    Change the height of User Profile Navbar Box, remove Php code inside of Index.html, add Card and Announcement View style, add Announcement Info page, change the Announcement Card code and design, add session data in Announcement Form page and Reindent the Layout file

--- src/pages/announcement/All_Announcement.js
+++ src/pages/announcement/All_Announcement.js
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AnnouncementCard from "../../components/announcement/Announcement_Card";
 import Layout from "../Layout";
+import Axios from 'axios';
+import defaultUserLogo from '../../images/avatars/default.jpg';
+import { Link } from "react-router-dom";
 
 export default function All_Announcement() {
+    const token = JSON.parse(sessionStorage.getItem('userToken'));
+    const [moreText, setMoreText] = useState(false);
+    const [showEditMenu, setShowEditMenu] = useState(null);
+    const [allAnnouncements, setAllAnnouncements] = useState([]);
+    const [showMoreOption, setShowMoreOption] = useState(false);
+
+    useEffect(() => {
+        const getAllAnnouncements = () => {
+            Axios.post('http://localhost:3001/announcement/get-all-announcements', {
+                authToken: token
+            }).then(res => {
+                if (res.statusText === 'OK') {
+                    console.log(res)
+                    setAllAnnouncements(res.data);
+                }
+            });
+        }
+        getAllAnnouncements()
+    }, [token])
     return (
         <Layout
             pageTitle="All Announcements"
             pageContent={
-                <div style={{ margin: "10px 0 0 100px"}}>
-                    <AnnouncementCard/>
+                <div className="announcementView">
+                    {allAnnouncements.map((data, index) => (
+                        <AnnouncementCard
+                            key={index}
+                            showMoreButtonOnClick={() => setShowMoreOption(currentShow => !currentShow)}
+                            className={`dropdown-modal ${showMoreOption == false ? '' : "open"}`}
+                            to={`/edit_announcement_form/${data.ANNOUNCEMENT_ID}`}
+                            src={data?.PICTURE == null ? {defaultUserLogo} : data?.PICTURE}
+                            title={data?.TITLE}
+                            body={data.BODY.length > 100 ? data?.BODY.substring(0, 100) + '...' : data?.BODY}
+                            showMoreButton={
+                                data.BODY.length > 100 &&
+                                <Link to={`/more_announcement/${data.ANNOUNCEMENT_ID}`}>show more</Link>
+                            }
+                        />
+                    ))
+                    }
                 </div>
             }
         />
